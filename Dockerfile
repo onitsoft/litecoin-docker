fa34157f9c8c8ba4f77166480ef9a5cbff432e7d
root <i.skinderis@gmail.com>
2019-08-06 13:37:17 +0000

    Dockerfile and start.sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM onitsoft/coin-base:latest
+LABEL maintainer="info@n.exchange"
+LABEL build_date="2019-08-06"
+LABEL description="v0.17.1"
 ADD app/ $COIN_BASE/app/
 RUN chmod -R +x $COIN_BASE/app/*
 RUN chown -R coin:coin $COIN_BASE
--- app/start.sh
+++ app/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 function gracefulShutdown {
 echo Shutting down!
-$COIN_BASE/app/litecoin-cli -conf=$COIN_BASE/data/LTC/config.conf -datadir=$COIN_BASE/data/LTC stop
+$COIN_BASE/app/litecoin-cli -conf=$COIN_BASE/data/config.conf -datadir=$COIN_BASE/data stop
 }
 trap gracefulShutdown SIGTERM
 sleep 5
-$COIN_BASE/app/litecoind -daemon=0 -conf=$COIN_BASE/data/LTC/config.conf -datadir=$COIN_BASE/data/LTC &
+$COIN_BASE/app/litecoind -daemon=0 -conf=$COIN_BASE/data/config.conf -datadir=$COIN_BASE/data &
 wait
 
